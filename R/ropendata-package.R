ffdead28a9214524cd13c8a96c057c438af4cc3f
brudis-r7 <bob@rud.is>
2018-07-17 17:28:28 -0400

    initial commit

---
+++ R/ropendata-package.R
@@ -0,0 +1,15 @@
+#' Query and Download 'Rapid7' Cybersecurity Data Sets
+#'
+#' 'Rapid7' collects cybersecurity data and makes it available via
+#' their 'Open Data' <opendata.rapid7.com> portal which has an 'API'. Tools are
+#' provided to assist in querying for available data sets and downloading any
+#' data set authorized to a registered account.
+#'
+#' @md
+#' @name ropendata
+#' @docType package
+#' @author Bob Rudis (bob_rudis@@rapid7.com)
+#' @import httr
+#' @importFrom utils download.file
+#' @importFrom jsonlite fromJSON
+NULL
